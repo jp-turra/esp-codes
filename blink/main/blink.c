3a80f12e5c56c3f237ef91978ad16139eb58de5f
Turra <joao.t06@hotmail.com>
2024-03-23 16:31:41 -0300

    Refactor FreeRTOS timer example to blink app

--- blink/main/blink.c
+++ blink/main/blink.c
@@ -8,10 +8,16 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/timers.h"
+
+#include "esp_log.h"
 
 #include "hal/gpio_types.h"
 #include "driver/gpio.h"
 
+#define TAG "blink_app"
+#define TIMER_ID 1
+#define BLINK_PERIOD 500 / portTICK_PERIOD_MS
 
 // Application headers
 
@@ -28,28 +34,45 @@ static void blink_led()
 {
     if (led_state == 0)
     {
-        printf("LED ON\n");
+        // ESP_LOGI(TAG, "LED ON");
         gpio_set_level(g_led_pin, 1);
-        led_state = 1;
     }
     else
     {
-        printf("LED OFF\n");
+        // ESP_LOGI(TAG, "LED OFF");
         gpio_set_level(g_led_pin, 0);
-        led_state = 0;
     }
 }
+
 static void blink_led_task(void* arg)
 {
     while (1)
     {
         blink_led();
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        // vTaskDelay(BLINK_PERIOD);
+        // led_state = !led_state;
     }
 }
-// Public functions
 
-void app_main(void)
+static void blink_increment_led_state(TimerHandle_t xTimer)
+{
+    configASSERT(xTimer);
+
+
+    ESP_LOGI(TAG, "Increment LED state");
+    uint8_t xtimer_id = (uint8_t) pvTimerGetTimerID(xTimer);
+    if (TIMER_ID == xtimer_id)
+    {
+        ESP_LOGI(TAG, "Increment LED state");
+        led_state = !led_state;
+    }
+    else
+    {
+        ESP_LOGW(TAG, "Invalid timer ID");
+    }
+}
+
+static void configure_led()
 {
     g_led_config.intr_type = GPIO_INTR_DISABLE;
     g_led_config.mode = GPIO_MODE_OUTPUT;
@@ -58,8 +81,42 @@ void app_main(void)
     g_led_config.pull_up_en = GPIO_PULLUP_DISABLE;
 
     gpio_config(&g_led_config);
+    ESP_LOGI(TAG, "Led configured");
+}
 
-    xTaskCreate(blink_led_task, "blink_led", 2048, NULL, 10, NULL); // 2048 bytes alocated for the task
+static void configure_timer()
+{
+    TimerHandle_t timer = xTimerCreate("led_timer", BLINK_PERIOD, pdTRUE, 
+                                        (void *) TIMER_ID, blink_increment_led_state);
+
+    if (timer == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to create timer");
+        abort();
+    }
+    else 
+    {
+        ESP_LOGI(TAG, "Timer created");
+        if (xTimerStart(timer, 0) != pdPASS)
+        {
+            ESP_LOGE(TAG, "Failed to start timer");
+            abort();
+        }
+    }
+}
+
+// Public functions
+
+void app_main(void)
+{
+    configure_led();
+
+    configure_timer();
+
+    ESP_LOGI(TAG, "Creating blink led task");
+    // Note: High priority task must be executed fast enough to not activate the watchdog.
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+    xTaskCreate(blink_led_task, "blink_led", 2048, NULL, 0, NULL); // 2048 bytes alocated for the task
 
     printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
 }
